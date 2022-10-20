088daf9c26904c80d9990bc4dfd77b56ba542403
Nishantphule <nishantphule12@gmail.com>
2022-10-20 13:54:10 +0530

    callback hell task

---
+++ js/script.js
@@ -0,0 +1,57 @@
+let content = document.getElementById("count")
+
+function Message() {
+    content.innerHTML = "Happy Independence Day !"
+    content.style.color = "red"
+}
+setTimeout(Message, 11000);
+
+function count10() {
+    content.innerHTML = "10"
+}
+setTimeout(count10, 1000);
+
+function count9() {
+    content.innerHTML = "9"
+}
+setTimeout(count9, 2000);
+
+function count8() {
+    content.innerHTML = "8"
+}
+setTimeout(count8, 3000);
+
+function count7() {
+    content.innerHTML = "7"
+}
+setTimeout(count7, 4000);
+
+function count6() {
+    content.innerHTML = "6"
+}
+setTimeout(count6, 5000);
+
+function count5() {
+    content.innerHTML = "5"
+}
+setTimeout(count5, 6000);
+
+function count4() {
+    content.innerHTML = "4"
+}
+setTimeout(count4, 7000);
+
+function count3() {
+    content.innerHTML = "3"
+}
+setTimeout(count3, 8000);
+
+function count2() {
+    content.innerHTML = "2"
+}
+setTimeout(count2, 9000);
+
+function count1() {
+    content.innerHTML = "1"
+}
+setTimeout(count1, 10000);
